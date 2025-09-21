324c1c295fb50807218183484809b76c33288032
divy151005 <a9076597253@gmail.com>
2025-09-21 17:50:42 +0530

    Created by Divya,Keshav,Jatin,Kartik,Dhruv

--- src/linkedlist.c
+++ src/linkedlist.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //
 // Created by rahul on 06-09-2025.
 //
@@ -55,11 +56,75 @@ void insertSinglyLinkedList(SinglyLinkedList* ll, int index, int value) {
     } else {
         Node* current = ll->head;
         for (int i = 0; i < index - 1; i++) {
+=======
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Node 
+{
+    int data;
+    struct Node* next;
+    struct Node* prev;
+} Node; // created by divya
+
+
+typedef struct 
+{
+    Node* head;
+    int size;
+} SinglyLinkedList; // created by divya
+
+
+typedef struct 
+{
+    Node* head;
+    Node* tail;
+    int size;
+} DoublyLinkedList; // created by divya
+
+
+typedef struct 
+{
+    Node* head;
+    int size;
+} CircularLinkedList; // created by divya
+
+
+SinglyLinkedList* createSinglyLinkedList() 
+{
+    SinglyLinkedList* list = (SinglyLinkedList*)malloc(sizeof(SinglyLinkedList));
+    if (!list) return NULL;
+    list->head = NULL;
+    list->size = 0;
+    return list;
+} // created by divya
+
+
+void insertSinglyLinkedList(SinglyLinkedList* list, int index, int value) 
+{
+    if (!list || index < 0 || index > list->size) return;
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (!newNode) return;
+    newNode->data = value;
+    newNode->next = NULL;
+    if (index == 0) 
+    {
+        newNode->next = list->head;
+        list->head = newNode;
+    } 
+    else 
+    {
+        Node* current = list->head;
+        for (int i = 0; i < index - 1; i++) 
+        {
+>>>>>>> d57cb21 (Created by Divya,Keshav,Jatin,Kartik,Dhruv)
             current = current->next;
         }
         newNode->next = current->next;
         current->next = newNode;
     }
+<<<<<<< HEAD
     ll->size++;
 }
 
@@ -73,12 +138,32 @@ void deleteSinglyLinkedList(SinglyLinkedList* ll, int index) {
     } else {
         Node* current = ll->head;
         for (int i = 0; i < index - 1; i++) {
+=======
+    list->size++;
+} // created by divya
+
+
+void deleteSinglyLinkedList(SinglyLinkedList* list, int index) 
+{
+    if (!list || index < 0 || index >= list->size) return;
+    Node* temp;
+    if (index == 0) 
+    {
+        temp = list->head;
+        list->head = list->head->next;
+    } else 
+    {
+        Node* current = list->head;
+        for (int i = 0; i < index - 1; i++) 
+        {
+>>>>>>> d57cb21 (Created by Divya,Keshav,Jatin,Kartik,Dhruv)
             current = current->next;
         }
         temp = current->next;
         current->next = temp->next;
     }
     free(temp);
+<<<<<<< HEAD
     ll->size--;
 }
 
@@ -97,19 +182,58 @@ void traverseSinglyLinkedList(SinglyLinkedList* ll) {
     Node* current = ll->head;
     printf("Singly Linked List: ");
     while (current != NULL) {
+=======
+    list->size--;
+} // created by divya
+
+
+void traverseSinglyLinkedList(SinglyLinkedList* list) 
+{
+    printf("Singly Linked List: ");
+    Node* current = list->head;
+    while (current) 
+    {
+>>>>>>> d57cb21 (Created by Divya,Keshav,Jatin,Kartik,Dhruv)
         printf("%d -> ", current->data);
         current = current->next;
     }
     printf("NULL\n");
+<<<<<<< HEAD
 }
 
 void freeSinglyLinkedList(SinglyLinkedList* ll) {
     Node* current = ll->head;
     while (current != NULL) {
+=======
+} // created by divya
+
+
+int hasCycleSinglyLinkedList(SinglyLinkedList* list) 
+{
+    if (!list || !list->head) return 0;
+    Node* slow = list->head;
+    Node* fast = list->head;
+    while (fast && fast->next) 
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) return 1;
+    }
+    return 0;
+} // created by divya
+
+
+void freeSinglyLinkedList(SinglyLinkedList* list) {
+    if (!list) return;
+    Node* current = list->head;
+    while (current) 
+    {
+>>>>>>> d57cb21 (Created by Divya,Keshav,Jatin,Kartik,Dhruv)
         Node* temp = current;
         current = current->next;
         free(temp);
     }
+<<<<<<< HEAD
     free(ll);
 }
 
@@ -213,19 +337,110 @@ void traverseDoublyLinkedList(DoublyLinkedList* dll) {
     Node* current = dll->head;
     printf("Doubly Linked List: ");
     while (current != NULL) {
+=======
+    free(list);
+} // created by divya
+
+
+DoublyLinkedList* createDoublyLinkedList() {
+    DoublyLinkedList* list = (DoublyLinkedList*)malloc(sizeof(DoublyLinkedList));
+    if (!list) return NULL;
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
+    return list;
+} // created by divya
+
+
+void insertDoublyLinkedList(DoublyLinkedList* list, int index, int value) 
+{
+    if (!list || index < 0 || index > list->size) return;
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (!newNode) return;
+    newNode->data = value;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    if (index == 0) 
+    {
+        newNode->next = list->head;
+        if (list->head) list->head->prev = newNode;
+        list->head = newNode;
+        if (!list->tail) list->tail = newNode;
+    } 
+    else if (index == list->size) 
+    {
+        newNode->prev = list->tail;
+        if (list->tail) list->tail->next = newNode;
+        list->tail = newNode;
+    } 
+    else 
+    {
+        Node* current = list->head;
+        for (int i = 0; i < index; i++) current = current->next;
+        newNode->next = current;
+        newNode->prev = current->prev;
+        if (current->prev) current->prev->next = newNode;
+        current->prev = newNode;
+    }
+    list->size++;
+} // created by keshav
+
+
+void deleteDoublyLinkedList(DoublyLinkedList* list, int index) {
+    if (!list || index < 0 || index >= list->size) return;
+    Node* temp = list->head;
+    if (index == 0) 
+    {
+        temp = list->head;
+        list->head = list->head->next;
+        if (list->head) list->head->prev = NULL;
+        if (list->tail == temp) list->tail = NULL;
+    } 
+    else 
+    {
+        for (int i = 0; i < index; i++) temp = temp->next;
+        if (temp->prev) temp->prev->next = temp->next;
+        if (temp->next) temp->next->prev = temp->prev;
+        if (list->tail == temp) list->tail = temp->prev;
+    }
+    free(temp);
+    list->size--;
+} // created by keshav
+
+
+void traverseDoublyLinkedList(DoublyLinkedList* list) 
+{
+    printf("Doubly Linked List: ");
+    Node* current = list->head;
+    while (current) 
+    {
+>>>>>>> d57cb21 (Created by Divya,Keshav,Jatin,Kartik,Dhruv)
         printf("%d <-> ", current->data);
         current = current->next;
     }
     printf("NULL\n");
+<<<<<<< HEAD
 }
 
 void freeDoublyLinkedList(DoublyLinkedList* dll) {
     Node* current = dll->head;
     while (current != NULL) {
+=======
+} // created by dhruv
+
+
+void freeDoublyLinkedList(DoublyLinkedList* list) 
+{
+    if (!list) return;
+    Node* current = list->head;
+    while (current) 
+    {
+>>>>>>> d57cb21 (Created by Divya,Keshav,Jatin,Kartik,Dhruv)
         Node* temp = current;
         current = current->next;
         free(temp);
     }
+<<<<<<< HEAD
     free(dll);
 }
 
@@ -287,10 +502,72 @@ void deleteCircularLinkedList(CircularLinkedList* cll, int index) {
         for (int i = 0; i < index - 1; i++) {
             current = current->next;
         }
+=======
+    free(list);
+} // created by dhruv
+
+
+CircularLinkedList* createCircularLinkedList() 
+{
+    CircularLinkedList* list = (CircularLinkedList*)malloc(sizeof(CircularLinkedList));
+    if (!list) return NULL;
+    list->head = NULL;
+    list->size = 0;
+    return list;
+} // created by kartik
+
+
+void insertCircularLinkedList(CircularLinkedList* list, int index, int value) 
+{
+    if (!list || index < 0 || index > list->size) return;
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (!newNode) return;
+    newNode->data = value;
+    if (!list->head) 
+    {
+        list->head = newNode;
+        newNode->next = newNode;
+    } 
+    else 
+    {
+        Node* current = list->head;
+        for (int i = 0; i < index - 1; i++) current = current->next;
+        newNode->next = current->next;
+        current->next = newNode;
+        if (index == 0) list->head = newNode;
+    }
+    list->size++;
+} // created by kartik
+
+
+void deleteCircularLinkedList(CircularLinkedList* list, int index) {
+    if (!list || index < 0 || index >= list->size) return;
+    Node* temp;
+    if (index == 0) 
+    {
+        if (list->size == 1) 
+        {
+            temp = list->head;
+            list->head = NULL;
+        } 
+        else 
+        {
+            temp = list->head;
+            Node* last = list->head;
+            while (last->next != list->head) last = last->next;
+            list->head = list->head->next;
+            last->next = list->head;
+        }
+    } else 
+    {
+        Node* current = list->head;
+        for (int i = 0; i < index - 1; i++) current = current->next;
+>>>>>>> d57cb21 (Created by Divya,Keshav,Jatin,Kartik,Dhruv)
         temp = current->next;
         current->next = temp->next;
     }
     free(temp);
+<<<<<<< HEAD
     cll->size--;
 }
 
@@ -319,10 +596,52 @@ void freeCircularLinkedList(CircularLinkedList* cll) {
 
     Node* current = cll->head->next;
     while (current != cll->head) {
+=======
+    list->size--;
+} // created by jatin
+
+
+void traverseCircularLinkedList(CircularLinkedList* list) 
+{
+    if (!list || !list->head) 
+    {
+        printf("Circular Linked List: Empty\n");
+        return;
+    }
+    Node* current = list->head;
+    printf("Circular Linked List: ");
+    int count = 0;
+    do 
+    {
+        printf("%d -> ", current->data);
+        current = current->next;
+        count++;
+    } 
+    while (current != list->head && count < list->size);
+    printf("(back to head)\n");
+} // created by jatin
+
+
+void freeCircularLinkedList(CircularLinkedList* list) {
+    if (!list || !list->head) 
+    {
+        free(list);
+        return;
+    }
+    Node* current = list->head->next;
+    while (current != list->head) 
+    {
+>>>>>>> d57cb21 (Created by Divya,Keshav,Jatin,Kartik,Dhruv)
         Node* temp = current;
         current = current->next;
         free(temp);
     }
+<<<<<<< HEAD
     free(cll->head);
     free(cll);
 }
+=======
+    free(list->head);
+    free(list);
+} // created by keshav
+>>>>>>> d57cb21 (Created by Divya,Keshav,Jatin,Kartik,Dhruv)
--- tests/test_linkedlist.c
+++ tests/test_linkedlist.c
@@ -1,3 +1,44 @@
 //
 // Created by rahul on 06-09-2025.
 //
+#include"..//src/linkedlist.c"
+int main() 
+{
+    SinglyLinkedList* sList = createSinglyLinkedList();
+    insertSinglyLinkedList(sList, 0, 10);
+    insertSinglyLinkedList(sList, 1, 20);
+    insertSinglyLinkedList(sList, 1, 15);
+    printf("After inserting 10, 15, 20:\n");
+    traverseSinglyLinkedList(sList);
+
+    deleteSinglyLinkedList(sList, 1);
+    printf("After deleting index 1 (15):\n");
+    traverseSinglyLinkedList(sList);
+    freeSinglyLinkedList(sList);
+
+    DoublyLinkedList* dList = createDoublyLinkedList();
+    insertDoublyLinkedList(dList, 0, 30);
+    insertDoublyLinkedList(dList, 1, 40);
+    insertDoublyLinkedList(dList, 1, 35);
+    printf("\nAfter inserting 30, 35, 40 (doubly):\n");
+    traverseDoublyLinkedList(dList);
+
+    deleteDoublyLinkedList(dList, 2);
+    printf("After deleting index 2 (40):\n");
+    traverseDoublyLinkedList(dList);
+    freeDoublyLinkedList(dList);
+
+    CircularLinkedList* cList = createCircularLinkedList();
+    insertCircularLinkedList(cList, 0, 50);
+    insertCircularLinkedList(cList, 1, 60);
+    insertCircularLinkedList(cList, 1, 55);
+    printf("\nAfter inserting 50, 55, 60 (circular):\n");
+    traverseCircularLinkedList(cList);
+
+    deleteCircularLinkedList(cList, 0);
+    printf("After deleting index 0 (50):\n");
+    traverseCircularLinkedList(cList);
+    freeCircularLinkedList(cList);
+
+    return 0;
+}
